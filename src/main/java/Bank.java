84442dad5f2fe558762b7f179612463990b936b1
Jay200028 <91471699+Jay200028@users.noreply.github.com>
2021-11-09 14:55:35 +0100

    Bankonto for spiller/player

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,26 @@
 public class Bank {
+// Konto på 1000
+
+    private int saldo;
+
+    public void Konto() {
+        saldo = 1000;
+    }
+
+    public boolean haev(int penge) {
+
+        if ((saldo + penge) < 0)
+            return false;
+
+        saldo = saldo + penge;
+        return true;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+
+    // Ændrer spillerens balance til 10000
+player.setBalance(10000);
 }
