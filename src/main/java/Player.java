aa175988ae5211b719e84ccb788ee6f4c1e163bc
MaiyarA <kiss.maiyar@hotmail.dk>
2021-11-09 14:37:13 +0100

    .

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
 public class Player {
+    public static void main(String[] args){
+        String player;
+        int totalPlayers = 4;
+        //Only 2-4 players
+
+        Scanner pl = new Scanner(System.in);
+        System.out.println("How many are playing?");
+        // -------
+
+        if (totalPlayers > 2){
+            System.out.println("End game");
+        }
+
+        else {
+            System.out.println("Enter name");
+            player= pl.nextLine();
+            System.out.println("Player = " + player);
+
+        }
+
+
+    }
 }
+
