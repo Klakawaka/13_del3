5ac885a13ef026bd43cff9177fc718db95fd25eb
Miladibra1 <91473417+Miladibra1@users.noreply.github.com>
2021-11-12 17:53:15 +0100

    ændringer på chancekort: Der er lavet 6 chancekort. -havde tilføjet en Main på chancekort for at teste klassen
    Ændringer på Player klassen:
    - har definerert balance og position - har tilføjet public void addBalance, addPosition og setPosition

--- src/main/java/ChanceCard.java
+++ src/main/java/ChanceCard.java
@@ -1,2 +1,59 @@
+import java.lang.Math;
 public class ChanceCard {
+        public static void main(String[] args) {
+                Player player1 = new Player();
+                Player player2 = new Player();
+                Player[] playerlist = new Player[2];
+                playerlist[0] = player1;
+                playerlist[1] = player2;
+                Player player = player1;
+
+                getChancecard(player,playerlist); //statisk miderlertidigt, slet når det instantieres (new Chancecard)
+
+
+        int tendollargift, tendollarloss, moveplayer3foward, moveplayer3back, movetojail;
+
+                //tendollargift =
+                //tendollarloss = 1
+                //moveplayer3foward = 2
+                //moveplayer3back = 3
+                //movetojail = 4
+
+
+        }
+        public static void getChancecard(Player player, Player[] playerlist)
+        {
+                int card = (int) (Math.random() * 6) + 1;
+                switch (card) {
+                        case 1:
+                                System.out.println("Du får en gave på 10 dollars");
+                                player.addBalance(10);
+
+                                break;
+                        case 2:
+                                System.out.println("Du mister 10 dollars");
+                                player.addBalance(-10);
+                                break;
+                        case 3:
+                                System.out.println("Du skal rykke 3 felter frem");
+                                player.addpostion(3);
+                                break;
+                        case 4:
+                                System.out.println("Du skal Rykke 3 felter tilbage");
+                                player.addpostion(-3);
+                                break;
+                        case 5:
+                                System.out.println("Du skal i fængsel i en runde");
+                                player.setPosition(17); //17 er bare eksempel, ændre til nummer for fængsel.
+                                break;
+                        case 6:
+                                System.out.println("du får 6 dollars af hver spiller");
+                                for (int i = 0; i < playerlist.length; i++) {
+                                        playerlist[i].addBalance(-6);
+                                }
+                                player.addBalance(playerlist.length * 6);
+                                break;
+                }
+        }
 }
+
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
 public class Player {
-    public static void main(String[] args){
-        String player;
-        int totalPlayers = 4;
-        // 2-4 players
 
+        String name;
+        int balance;
+        int position;
+
+            //adbalance bruges til case 1,2 og 6//
+        public void addBalance(int toAddBalance){
+            balance = balance + toAddBalance;
+        }
+            //adposition bruges til case 3 og 4
+        public void addpostion(int toAddPosition){
+            position = position + toAddPosition;
+        }
+            //setposition bruges til case 5, i case 5 rykkes man til fængsel. Nummeret for dette felt skal ændres
+            //under chancekort-klassen i player.setPosition(x)//
+
+        public void setPosition(int moveto){
+            position = moveto;
+        }
+
+    public void toMain(){
         Scanner pl = new Scanner(System.in);
         System.out.println("How many are playing?");
         // -------
@@ -15,12 +31,13 @@ public class Player {
 
         else {
             System.out.println("Enter name");
-            player= pl.nextLine();
-            System.out.println("Player = " + player);
+            String playername = pl.nextLine();
+            Player player1 = new Player();
+            System.out.println("Player = " + playername);
 
         }
 
-
     }
+
 }
 
