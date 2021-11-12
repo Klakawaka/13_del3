c79a15e733ded344554ae25f28efba9574ada016
Mudi26 <s215772@student.dtu.dk>
2021-11-12 10:10:20 +0100

    Oversat bynavne til engelsk (Gameboard) + fixet klasse navnet

--- src/main/java/Gameboard.java
+++ src/main/java/Gameboard.java
@@ -1,18 +1,18 @@
-public class Gamebord {
-    int Nairobi, Tripoli, Bukarest, Brasilia, Athen, Prag, Madrid, Paris, København;
-    int Stockholm, Rom, Dublin, Singapore, Montreal, NewYork, Dubai;
+public class Gameboard {
+    int Nairobi, Tripoli, Bukarest, Brasilia, Athen, Prague, Madrid, Paris,Copenhagen;
+    int Stockholm, Rome, Dublin, Singapore, Montreal, NewYork, Dubai;
     public void value() {
         Nairobi = 60;
         Tripoli = 65;
         Bukarest = 85;
         Brasilia = 95;
         Athen = 120;
-        Prag = 145;
+        Prague = 145;
         Madrid = 180;
         Paris = 190;
-        København = 210;
+        Copenhagen = 210;
         Stockholm = 220;
-        Rom = 270;
+        Rome = 270;
         Dublin = 285;
         Singapore = 325;
         Montreal = 340;
