b3582830d580223fe04b37611b86897f8a342cdf
Staximus <91071419+Staximus@users.noreply.github.com>
2021-11-22 23:57:09 +0100

    GUI Relateret: Indsat få rettelser på beskrivelser og farve på tekst.

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -13,7 +13,6 @@ public class Gui {
     int[] playerlistPosition;
     ChanceCard chance = new ChanceCard();
 
-
         //Dice dice1,dice2; Del af vores gamle kode.
 
 
@@ -31,23 +30,23 @@ public class Gui {
                 new GUI_Street("Bucharest", "Price: 85 $", "This is the capital of Romania. Population: 3.8 Million", "85", Color.green, Color.black),
                 new GUI_Street("Brasilia", "Price: 95 $", "This is the capital of.. you guessed it! Brazil! Population: 4.7 Million", "95", Color.GREEN, Color.black),
                 new GUI_Jail("default", "Jail", "Visiting Jail", "You're visiting", Color.gray, Color.black),
-                new GUI_Street("Athene", "Price: 120 $", "This is the capital of Greece. Population: 664 Thousands", "120", Color.YELLOW, Color.black),
+                new GUI_Street("Athene", "Price: 120 $", "This is the capital of Greece. Population: 664 Thousand", "120", Color.YELLOW, Color.black),
                 new GUI_Street("Prague", "Price: 145 $", "This is the capital of the Czech Republic. Population: 1.3 Million", "145", Color.yellow, Color.black),
                 new GUI_Chance("?", "Try ur Luck!", "You landed on ''?'' This field yield many random possibilities!", Color.orange, Color.black),
                 new GUI_Street("Madrid", "Price: 180 $", "This is the capital of Spain. Population: 3.07 Million", "180", Color.pink, Color.black),
-                new GUI_Street("Paris", "Price: 190 $", "This is the capital of France.", "190", Color.pink, Color.black),
+                new GUI_Street("Paris", "Price: 190 $", "This is the capital of France. Population: 2.1 Million.", "190", Color.pink, Color.black),
                 new GUI_Refuge("default", "Parkingspot", "FREE PARK", "This is a parkingspot which allows you to park here for free! Plus add all uncollected fines to your own bank deposit!", Color.gray, Color.black),
                 new GUI_Street("Copenhagen", "Price 210 $", "This is the capital of Denmark. Population: 602 Thousand.", "210", Color.white, Color.black),
                 new GUI_Street("Stockholm", "Price: 220 $", "This is the capital of Sweden. Population: 975 Thousand. ", "220", Color.white, Color.black),
                 new GUI_Chance("?", "Try ur Luck!", "You landed on ''?'' This field yield many random possibilities!", Color.orange, Color.black),
-                new GUI_Street("Rome", "Price: 270 $ ", "This is the capital of Italy.", "270", Color.darkGray, Color.white),
+                new GUI_Street("Rome", "Price: 270 $ ", "This is the capital of Italy. Population: 2.8 Million.", "270", Color.darkGray, Color.white),
                 new GUI_Street("Dublin", "Price: 285 $", "This is the capital of Ireland. Population: 554 Thousand.", "285", Color.darkGray, Color.white),
                 new GUI_Jail("default", "Jail", "Go to JAIL", "You have been arrested, you need to go to jail...", Color.gray, Color.black),
                 new GUI_Street("Singapore", "Price: 325 $", "This is the capital of Singapore! Singapore is both a city and a country as it is a citystate governed on its own. Population: 5.6 Million.", "325", Color.magenta, Color.black),
                 new GUI_Street("Montreal", "Price: 340 $", "This is the second biggest city of Canada. Population: 1.7 Million.", "340", Color.magenta, Color.black),
                 new GUI_Chance("?", "Try ur Luck!", "You landed on ''?'' This field yield many random possibilities!", Color.orange, Color.black),
-                new GUI_Street("Dubai", "Price: 420 $", "This is the largest city of the United Arab Emirates. Population: 3.3 Million.", "420", Color.BLUE, Color.black),
-                new GUI_Street("New york", "Price: 450 $", "This is the biggest city of the United States. Population: 8.2 Million.", "450", Color.BLUE, Color.black),
+                new GUI_Street("Dubai", "Price: 420 $", "This is the largest city of the United Arab Emirates. Population: 3.3 Million.", "420", Color.BLUE, Color.white),
+                new GUI_Street("New york", "Price: 450 $", "This is the biggest city of the United States. Population: 8.2 Million.", "450", Color.BLUE, Color.white),
         };
         gui = new GUI(fields, Color.CYAN); //initilize gui variable
     }
