0f428de9bf50ac8f0500bef12ba6bc2ba174a42d
Mudi26 <s215772@student.dtu.dk>
2021-11-23 16:56:45 +0100

    Vi har tilføjet en klasse med JUnit testen (DiceTest)

---
+++ src/test/DiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DiceTest {
+
+    @Test
+    public void test(){
+        Dice die = new Dice();
+        for (int i = 0; i < 50; i++) {
+            int faceValue = die.roll();
+            assertTrue(faceValue>=1);
+            assertTrue(faceValue<=6);
+        }
+    }
+}
