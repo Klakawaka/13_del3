d59d58b1c9f19fd75cc06d9b0c21ba6d8b128c66
Staximus <91071419+Staximus@users.noreply.github.com>
2021-11-26 21:38:45 +0100

    Prøver at få field til at lykkedes..

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -9,16 +9,16 @@ public class Field {
 
         GUI gui = new GUI(); {
 
-        // Sætter Albert som ejeren på Øresund-redderiet
+
         ((GUI_Ownable) gui.getFields()[5]).setOwnerName("Player");
 
-        // Sætter kantfarven på feltet
+
         ((GUI_Ownable) gui.getFields()[5]).setBorder(Color.GREEN);
 
-        // Sætter lejen af Øresunds redderiet til 1000
+
         ((GUI_Ownable) gui.getFields()[5]).setRent("60");
 
-        // Ændrer prisen for Rødovrevej til 500,-
+
         ((GUI_Ownable) gui.getFields()[1]).setSubText("Price: 60");
     }
 }
